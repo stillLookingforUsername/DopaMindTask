fae1658afc08f7f26350bfbbe68fc6cca72b62d0
bankimRabha <144623987+stillLookingforUsername@users.noreply.github.com>
2025-11-18 01:00:11 +0530

    JumpFreezeArea Script Added

--- Assets/Scripts/JumpFreezeArea.cs
+++ Assets/Scripts/JumpFreezeArea.cs
@@ -1,18 +1,66 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
+using UnityEngine.UI;
 
 public class JumpFreezeArea : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public GameObject jumpUI; //World space canvas
+    public Transform lookTarget; //point on the wall to face
+    public TextMeshPro jumpCounterText; //reference to JumpCounter text  inside  the jumpUI
+
+    private bool active = false;
+    private int jumpCount = 0;
+
+    private void OnTriggerEnter(Collider other)
     {
-        
+        if(!other.TryGetComponent<PlayerMovement>(out PlayerMovement player)) return;
+
+        //disable normal movement and enable jump only movement
+        var pm = other.GetComponent<PlayerMovement>();
+        if(pm != null) pm.enabled = false;
+
+        var jo = other.GetComponent<JumpOnly>();
+        if(jo != null) jo.enabled = true;
+
+        //force player to look at the jump info
+        other.transform.LookAt(lookTarget);
+        jumpUI.SetActive(true);
+        active = true;
+        UpdateCounter();
     }
 
-    // Update is called once per frame
-    void Update()
+    private void OnTriggerExit(Collider other)
     {
+        if(!other.TryGetComponent<PlayerMovement>(out PlayerMovement player)) return;
         
+        var pm = other.GetComponent<PlayerMovement>();
+        if(pm != null) pm.enabled = true;
+
+        var jo = other.GetComponent<JumpOnly>();
+        if(jo != null) jo.enabled = false;
+
+        jumpUI.SetActive(false);
+        active = false;
     }
+
+    private void Update()
+    {
+        if(!active) return;
+        if(Input.GetKeyDown(KeyCode.Space))
+        {
+            jumpCount++;
+            UpdateCounter();
+        }
+    }
+
+    private void UpdateCounter()
+    {
+        if(jumpCounterText != null)
+        {
+            jumpCounterText.text = "Jumps Count: " + jumpCount.ToString();
+        }
+    }
+
 }
