3af48907c5d5bd2a354441b76b9665e3ad372b06
bankimRabha <144623987+stillLookingforUsername@users.noreply.github.com>
2025-11-17 18:12:39 +0530

    TVController

---
+++ Assets/Scripts/TVController.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TVController : MonoBehaviour
+{
+    public GameObject screenSprite;
+    public AudioSource audioSource;
+    public GameObject ui;
+
+    private bool isOn = false;
+    private bool isNear = false;
+
+    private void Update()
+    {
+        if(isNear && Input.GetKeyDown(KeyCode.E))
+        {
+            ToggleTV();
+        }
+    }
+    private void ToggleTV()
+    {
+        isOn = !isOn;
+        screenSprite.SetActive(isOn);
+
+        if (isOn) audioSource.Play();
+        else audioSource.Stop();
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.TryGetComponent<PlayerMovement>(out PlayerMovement player))
+        {
+            isNear = true;
+            ui.SetActive(true);
+        }
+    }
+    private void OnTriggerExit(Collider other)
+    {
+        if (!other.TryGetComponent<PlayerMovement>(out PlayerMovement player)) return;
+        isNear = false;
+        ui.SetActive(false);
+    }
+}
